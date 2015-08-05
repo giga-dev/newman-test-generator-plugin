9c2f93a6dbe63a40fd9d3b9631f0e8e1abd8e738
yohana <yohaha@yohanakhoury.com>
2015-08-05 23:03:19 +0300

    Added scanner for SGTest

---
+++ src/main/java/com/gigaspaces/newman/testgenerator/scanner/TestNGScanner.java
@@ -0,0 +1,77 @@
+package com.gigaspaces.newman.testgenerator.scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Kobi Kisos, Yohana Khoury
+ * @since 10.2
+ */
+public class TestNGScanner extends AbstractNewmanScanner {
+    @Override
+    public boolean isFilteredByAnnotations(JSONObject annotations) {
+        return annotations.get("org.testng.annotations.Test.enabled").equals("false");
+    }
+
+    @Override
+    public Class getTestAnnotationClass() {
+        return org.testng.annotations.Test.class;
+    }
+
+    @Override
+    public JSONObject scanAndGet(String type, FilterBuilder filter, Set<URL> urls) throws Exception {
+
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setScanners(new SubTypesScanner(false), new MethodAnnotationsScanner(), new ResourcesScanner())
+                .setUrls(urls)
+                .filterInputsBy(filter));
+
+        Set<Method> methods = new HashSet<Method>();
+        Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+        for (Class clazz : classes) {
+            if (Modifier.isAbstract(clazz.getModifiers())) {
+                continue;
+            }
+
+            Method[] methods1 = clazz.getDeclaredMethods();
+            for (Method method : methods1) {
+                Class klass = clazz;
+                while (klass != Object.class && klass != null) {
+                    try {
+                        Method klassMethod = klass.getDeclaredMethod(method.getName(), method.getParameterTypes());
+                        if (klassMethod.isAnnotationPresent(getTestAnnotationClass())) {
+                            methods.add(method);
+                            break;
+                        }
+                    } catch (NoSuchMethodException e) {
+                        break;
+                    }
+                    klass = klass.getSuperclass();
+                }
+            }
+        }
+
+        System.out.println("Total: " + methods.size());
+        JSONArray testsJSON = scanMethods(methods);
+        System.out.println("Type: " + type);
+        JSONObject object = new JSONObject();
+        object.put("type", type);
+        object.put("tests", testsJSON);
+        return object;
+    }
+}
